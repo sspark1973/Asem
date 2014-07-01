687dbed7b4f225aec96328e474dbed5e571db9b8
sspark1973 <sspark1973@gmail.com>
2014-07-01 16:59:47 -0700

    Update hello2.s

--- hello2.s
+++ hello2.s
@@ -1,48 +1,22 @@
-@ Hello World example in ARM assembly language
-@ ============================================
-@ Using GCC to compile it, the @ sign seems
-@  to work best for line comments
-@ compile using something like:
-@  (arm-linux-)gcc -o helloarm helloarm.S -nostdlib
-@  with -DEABI for the newer EABI
-@    or -DTHUMB for 16-bit thumb code
-@  (if the kernel supports these modes)
-
-#ifdef THUMB
-.code 16
-#endif
-
-.text                 @ section declaration
-    .align  2         @ word boundary (2^2)
-    .global _start    @ export the entry point to
-                      @ the ELF linker or loader
-                      @ (like in crt1.o)
-#ifdef THUMB
-    .thumb_func
-#endif
-_start:
-
-@       write string to stdout
-@
-        mov    r0,#1      @ r0 file handle (stdout)
-        adr    r1,msg     @ r1 ptr to string
-        mov    r2,#12     @ r2 string length
-#if defined(EABI) || defined(THUMB)
-        mov    r7,#4
-        swi    0x00       @ call kernel
-#else
-        swi    0x0900004  @ call kernel
-#endif
-
-@       exit back to system
-@
-        mov    r0,#0      @ r0 exit code
-#if defined(EABI) || defined(THUMB)
-        mov    r7,#1
-        swi    0x00       @ call kernel
-#else
-        swi    0x0900001  @ call kernel
-#endif
-
-msg:
-    .ascii     "Hello World\n"
+# switch to a nicer syntax
+.syntax unified
+ 
+# declare main as exportable (public)
+.global main
+ 
+# our main "function"
+main:
+        # push return address (lr) and ip on the stack
+        push {ip, lr}
+        # put the address of the string into the r0 register
+        ldr r0, =message
+        # branch local - a function call, printf()
+        bl printf
+        # put 0 in r0 as result of the main function (return 0)
+        mov r0, #0
+        # pop the values from stack into ip and pc registers
+        pop {ip, pc}
+ 
+# our string to print, ASCII zero terminated
+message:
+        .asciz "Hello world!\n"
