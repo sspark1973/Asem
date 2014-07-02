67795a2a70f9eb715d072860a6ff6ee395576c95
sspark1973 <sspark1973@gmail.com>
2014-07-01 20:28:38 -0700

    Create gcchello.c

---
+++ gcchello.c
@@ -0,0 +1,12 @@
+    .syntax unified
+    
+.global main 
+main:
+    push    {ip, lr}
+    ldr     r0, =message
+    bl      printf
+    mov     r0, #0
+    pop     {ip, pc}
+    
+message:
+    .asciz "Hello, world.\n"
