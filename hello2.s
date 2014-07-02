8a89b78cd62c17d9d7ec5b3b591eea9eec79a217
sspark1973 <sspark1973@gmail.com>
2014-07-01 17:27:57 -0700

    Update hello2.s

--- hello2.s
+++ hello2.s
@@ -1,22 +1,22 @@
-# switch to a nicer syntax
-# .syntax unified
- 
-# declare main as exportable (public)
-.global main
- 
-# our main "function"
-main:
-        # push return address (lr) and ip on the stack
-        push {ip, lr}
-        # put the address of the string into the r0 register
-        ldr r0, =message
-        # branch local - a function call, printf()
-        bl printf
-        # put 0 in r0 as result of the main function (return 0)
-        mov r0, #0
-        # pop the values from stack into ip and pc registers
-        pop {ip, pc}
- 
-# our string to print, ASCII zero terminated
-message:
-        .asciz "Hello world!\n"
+.data
+
+msg:
+.ascii "Hello, ARM World!\n"
+len = . - msg
+
+
+.text
+
+.globl _start
+_start:
+/* write syscall */
+mov %r0, $1 
+ldr %r1, =msg 
+ldr %r2, =len 
+mov %r7, $4 
+swi $0 
+
+/* exit syscall */
+mov %r0, $0 
+mov %r7, $1 
+swi $0
