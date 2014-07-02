22368afb3389f9ee0490b7debe3e06f2f28342e9
sspark1973 <sspark1973@gmail.com>
2014-07-01 20:03:47 -0700

    Update hello2.s

--- hello2.s
+++ hello2.s
@@ -1,12 +1,41 @@
-.global _start
-_start:
-  MOV R7, #4
-  MOV R0, #1
-  MOV R2, #12
-  LDR R1, =string
-  SWI 0
-  MOV R7, #1
-  SWI 0
-  .data
-string:
-  .ascii "Hello World\n"
+            ; Hello world in ARM assembler
+
+            AREA text, CODE
+            ; This section is called "text", and contains code
+
+            ENTRY
+
+            ; Print "Hello world"
+
+            ; Get the offset to the string in r4.
+            adr       r4, hello           ;; "address in register"
+
+loop        ; "loop" is a label and designates an address
+            ; Call putchar to display each character
+            ; to illustrate how a loop works
+
+            ldrb      r0, [r4], #1        ; Get next byte and post-index r4
+            cmp       r0, #0              ; Stop when we hit a null
+            beq       outputstring        ;; "branch if equal" = cond. goto
+
+            bl        putchar             
+            b         loop                ;; "branch" =  goto
+
+outputstring
+            ; Alternatively, use putstring to write out the
+            ; whole string in one go
+            adr       r0, hello
+            bl        putstring           ;; "branch+link" = subroutine call
+
+finish
+            ; Standard exit code: SWI 0x123456, calling routine 0x18
+            ; with argument 0x20026
+            mov       r0, #0x18
+            mov       r1, #0x20000        ; build the "difficult" number...
+            add       r1, r1, #0x26       ; ...in two steps
+            SWI       0x123456            ;; "software interrupt" = sys call
+
+hello
+            DCB       "Hello World\n",0
+
+            END
